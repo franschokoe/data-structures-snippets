0bd6c39a7a335ff43c9281e2317ec638500e7f68
Frans Chokoe <fmoyagabo@gmail.com>
2026-04-09 05:11:12 +0200

    done commit

--- src/TestArrayandLinkedList/TestArrayandLinkedList.java
+++ src/TestArrayandLinkedList/TestArrayandLinkedList.java
@@ -13,8 +13,29 @@ public class TestArrayandLinkedList {
         arrayList.add(3 , 30);
 
         //output
+        System.out.println("A list of integer in the array list:");
+        System.out.println(arrayList);
 
 
+        LinkedList<Object> linkedList = new LinkedList<>(arrayList);
+        linkedList.add(1, "red");
+        linkedList.removeLast();
+        linkedList.addFirst("green");
 
+        System.out.println("Display the linked list forward:");
+        ListIterator<Object> listIterator = linkedList.listIterator();
+
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("Display the linked list backwards:");
+        listIterator = linkedList.listIterator(linkedList.size());
+
+        //loop
+        while(listIterator.hasPrevious()){
+            System.out.print(listIterator.previous() + " ");
+        }
     }
 }
