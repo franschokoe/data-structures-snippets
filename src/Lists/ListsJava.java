5166001a6b45df98a8f3fa19231a395082661f88
Frans Chokoe <fmoyagabo@gmail.com>
2026-04-09 03:23:51 +0200

    List done

--- src/Lists/ListsJava.java
+++ src/Lists/ListsJava.java
@@ -1,2 +1,22 @@
+package Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class ListsJava {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+
+        //iterator
+        //time complexity O(n)
+        for (int i = 0 ; i < list.size(); i++){
+            System.out.print(list.remove(i));
+        }
+    }
 }
