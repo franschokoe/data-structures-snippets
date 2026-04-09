102121a5a5e055b6d86feb26b69b5b14a1e2599f
Frans Chokoe <fmoyagabo@gmail.com>
2026-04-09 04:50:37 +0200

    Array & LinkedList

---
+++ src/TestArrayandLinkedList/TestArrayandLinkedList.java
@@ -0,0 +1,20 @@
+package TestArrayandLinkedList;
+import java.util.*;
+
+public class TestArrayandLinkedList {
+    public static void main(String[] args){
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(1);
+        arrayList.add(4);
+        arrayList.add(0,30);
+        arrayList.add(3 , 30);
+
+        //output
+
+
+
+    }
+}
