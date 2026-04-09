0d1f964f23fb7178ed590c70d16d1099f8724218
Frans Chokoe <fmoyagabo@gmail.com>
2026-04-09 05:13:35 +0200

    readme

--- src/TestArrayandLinkedList/TestArrayandLinkedList.java
+++ src/TestArrayandLinkedList/TestArrayandLinkedList.java
@@ -39,3 +39,11 @@ public class TestArrayandLinkedList {
         }
     }
 }
+
+
+//A list of integer in the array list:
+//        [30, 1, 2, 30, 3, 1, 4]
+//Display the linked list forward:
+//green 30 red 1 2 30 3 1
+//Display the linked list backwards:
+//        1 3 30 2 1 red 30 green
