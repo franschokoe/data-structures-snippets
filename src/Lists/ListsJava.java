2048a5f53bfbd1cd059d2b2422318f1b3b2d3e2e
Frans Chokoe <fmoyagabo@gmail.com>
2026-04-09 03:34:48 +0200

    yhoo

--- src/Lists/ListsJava.java
+++ src/Lists/ListsJava.java
@@ -12,11 +12,13 @@ public class ListsJava {
         list.add("B");
         list.add("C");
         list.add("D");
-
+//        System.out.print(list.indexOf("A"));
+//        System.out.print(list.size());
         //iterator
         //time complexity O(n)
         for (int i = 0 ; i < list.size(); i++){
-            System.out.print(list.remove(i));
+            System.out.print(list.remove(i) + "\n");
+            System.out.print(list + "\n");
         }
     }
 }
